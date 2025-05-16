f14007803039fd99412747e7f0f383ffb2788687
Zwelithini Golowa <zweli@Zwelithinis-MacBook-Pro.local>
2025-05-16 16:00:26 +0200

    fixed download timesheet

--- app/timesheets/reports/page.js
+++ app/timesheets/reports/page.js
@@ -7,14 +7,28 @@ import { LoadingContext } from "../../../components/LoadingContext";
 import { fetchMyTimesheets } from "../../api/api";
 import * as XLSX from "xlsx";
 import html2pdf from "html2pdf.js";
-import {  useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { set } from "date-fns";
-import { Typography, Table, TableBody, TableCell, TextField, TableHead, TableRow, Button, Dialog, DialogTitle, DialogContent, IconButton, MenuItem } from "@mui/material";
-import InfoIcon from '@mui/icons-material/Info';
-import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import {
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TextField,
+  TableHead,
+  TableRow,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+  MenuItem,
+} from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import DownloadForOfflineIcon from "@mui/icons-material/DownloadForOffline";
 import ViewTimesheet from "../../../components/ViewTimesheet";
 
-import TablePagination from '@mui/material/TablePagination';
+import TablePagination from "@mui/material/TablePagination";
 const TimesheetReports = () => {
   //declaring session and other variables
   const { data: session, status } = useSession({ required: true });
@@ -35,8 +49,8 @@ const TimesheetReports = () => {
   const [openDialog, setOpenDialog] = useState(false);
   const [viewedTimesheet, setViewedTimesheet] = useState(null);
   const [viewedpageTimesheet, setViewedpageTimesheet] = useState("download");
-  const [periodFilter, setPeriodFilter] = useState('');
-  const [districtFilter, setDistrictFilter] = useState('');
+  const [periodFilter, setPeriodFilter] = useState("");
+  const [districtFilter, setDistrictFilter] = useState("");
 
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -96,25 +110,38 @@ const TimesheetReports = () => {
     const search = searchTerm.toLowerCase();
     return (
       (entry.employee_id.toString().includes(search) ||
-      entry.staff_name.toLowerCase().includes(search) ||
-      entry.department.toLowerCase().includes(search) ||
-      entry.district.toLowerCase().includes(search) ||
-      entry.period.toLowerCase().includes(search)) &&
-      (districtFilter === '' || entry.district.toLowerCase() === districtFilter.toLowerCase()) &&
-      (periodFilter === '' || entry.period.toLowerCase().includes(periodFilter.toLowerCase()))
+        entry.staff_name.toLowerCase().includes(search) ||
+        entry.department.toLowerCase().includes(search) ||
+        entry.district.toLowerCase().includes(search) ||
+        entry.period.toLowerCase().includes(search)) &&
+      (districtFilter === "" ||
+        entry.district.toLowerCase() === districtFilter.toLowerCase()) &&
+      (periodFilter === "" ||
+        entry.period.toLowerCase().includes(periodFilter.toLowerCase()))
     );
   });
 
-
   const handleDownload = async (entry) => {
     try {
-      const response = await fetch(`http://127.0.0.1:8000/api/timesheets/${entry.id}/`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
+      const response = await axiosInstance.get(
+        `/timesheets/${entry.id}/`,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // const response = await fetch(
+      //   `http://127.0.0.1:8000/api/timesheets/${entry.id}/`,
+      //   {
+      //     method: "GET",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   }
+      // );
+      const data = await response.data;
       setViewedTimesheet(data);
       window.setTimeout(() => {
         const downloadelement = document.getElementById("content");
@@ -128,11 +155,15 @@ const TimesheetReports = () => {
             html2canvas: { scale: 2 },
             jsPDF: { unit: "mm", format: "a3", orientation: "landscape" },
           };
-  
-          html2pdf().set(opt).from(downloadelement).save().then(() => {
-            downloadelement.classList.remove("visible");
-            downloadelement.classList.add("hidden");
-          });
+
+          html2pdf()
+            .set(opt)
+            .from(downloadelement)
+            .save()
+            .then(() => {
+              downloadelement.classList.remove("visible");
+              downloadelement.classList.add("hidden");
+            });
         } else {
           console.error("PDF content not found");
         }
@@ -153,10 +184,25 @@ const TimesheetReports = () => {
       return;
     }
     const filteredData = timesheets.filter((entry) => {
-      const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
       const formatPeriod = (period) => {
         const [monthName, year] = period.split(" ");
-        const month = (monthNames.indexOf(monthName) + 1).toString().padStart(2, '0');
+        const month = (monthNames.indexOf(monthName) + 1)
+          .toString()
+          .padStart(2, "0");
         return `${year}-${month}`;
       };
       const periodDate = formatPeriod(entry.period);
@@ -171,12 +217,18 @@ const TimesheetReports = () => {
     setFilteredData(filteredData);
     if (filteredData.length > 0) {
       const fileName = `Timesheet Report_${filters.district} (${filters.startDate} to ${filters.endDate})`;
-      console.log(filteredData)
+      console.log(filteredData);
       if (filters.format === "excel") {
-        const projects = filteredData[0].project ? Object.keys(filteredData[0].project) : [];
+        const projects = filteredData[0].project
+          ? Object.keys(filteredData[0].project)
+          : [];
         const worksheetData = filteredData.map((entry) => {
-          const projectHours = projects.map((project) => entry.project[project].hours);
-          const projectLoe = projects.map((project) => entry.project[project].loe);
+          const projectHours = projects.map(
+            (project) => entry.project[project].hours
+          );
+          const projectLoe = projects.map(
+            (project) => entry.project[project].loe
+          );
           return [
             entry.employee_id,
             entry.staff_name,
@@ -222,18 +274,22 @@ const TimesheetReports = () => {
               margin: 0.1,
               filename: `${fileName}.pdf`,
               image: { type: "jpeg", quality: 0.98 },
-              html2canvas: { scale: 2},
+              html2canvas: { scale: 2 },
               jsPDF: {
                 unit: "mm",
                 format: "a4",
                 orientation: "potrait",
               },
-              pagebreak: { mode: 'avoid-all' },
+              pagebreak: { mode: "avoid-all" },
             };
-            html2pdf().set(opt).from(element).save().then(() => {
-              element.classList.remove("visible");
-              element.classList.add("hidden");
-            });
+            html2pdf()
+              .set(opt)
+              .from(element)
+              .save()
+              .then(() => {
+                element.classList.remove("visible");
+                element.classList.add("hidden");
+              });
           } else {
             console.error("PDF content not found");
           }
@@ -244,87 +300,113 @@ const TimesheetReports = () => {
     }
   };
 
-
-
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
-  
+
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
 
-
-  const paginatedTimesheets = filteredTimesheets.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  const paginatedTimesheets = filteredTimesheets.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
   return (
     <div>
       <Dialog open={openDialog} onClose={handleCloseDialog}>
         <DialogTitle>Export Timesheet Report</DialogTitle>
         <DialogContent>
-          <TimesheetReport handleFilterChange={handleFilterChange} handleExport={handleExport} filters={filters} districts={districts} />
+          <TimesheetReport
+            handleFilterChange={handleFilterChange}
+            handleExport={handleExport}
+            filters={filters}
+            districts={districts}
+          />
         </DialogContent>
       </Dialog>
-      <Typography variant="h5" gutterBottom sx={{ fontWeight: "700", marginTop: "10px" }}>
+      <Typography
+        variant="h5"
+        gutterBottom
+        sx={{ fontWeight: "700", marginTop: "10px" }}
+      >
         Timesheet Report
       </Typography>
-     
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <Button variant="contained" onClick={handleShowExport}> Export Reports </Button>
-        <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center", gap: "10px" }}>
-  <TextField
-    label="Search"
-    placeholder="Search by ID, Name or Department"
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-    sx={{ width: "300px" }}
-    inputProps={{ style: { textAlign: "left" } }}
-  />
-  <TextField
-    select
-    label="District"
-    value={districtFilter}
-    onChange={(e) => setDistrictFilter(e.target.value)}
-    sx={{ width: "150px" }}
-  >
-    <MenuItem value="">All</MenuItem>
-  {districts.map((district, index) => (
-    <MenuItem key={index} value={district.name}>
-      {district.name}
-    </MenuItem>
-  ))}
-  </TextField>
-  <TextField
-    select
-    label="Period"
-    value={periodFilter}
-    onChange={(e) => setPeriodFilter(e.target.value)}
-    sx={{ width: "150px" }}
-  >
-    <MenuItem value="">All</MenuItem>
-    {[...new Set(timesheets.map((timesheet) => timesheet.period))].map((period, index) => (
-      <MenuItem key={index} value={period}>
-        {period}
-      </MenuItem>
-    ))}
-  </TextField>
- 
-</div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Button variant="contained" onClick={handleShowExport}>
+          {" "}
+          Export Reports{" "}
+        </Button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <TextField
+            label="Search"
+            placeholder="Search by ID, Name or Department"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            sx={{ width: "300px" }}
+            inputProps={{ style: { textAlign: "left" } }}
+          />
+          <TextField
+            select
+            label="District"
+            value={districtFilter}
+            onChange={(e) => setDistrictFilter(e.target.value)}
+            sx={{ width: "150px" }}
+          >
+            <MenuItem value="">All</MenuItem>
+            {districts.map((district, index) => (
+              <MenuItem key={index} value={district.name}>
+                {district.name}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            select
+            label="Period"
+            value={periodFilter}
+            onChange={(e) => setPeriodFilter(e.target.value)}
+            sx={{ width: "150px" }}
+          >
+            <MenuItem value="">All</MenuItem>
+            {[...new Set(timesheets.map((timesheet) => timesheet.period))].map(
+              (period, index) => (
+                <MenuItem key={index} value={period}>
+                  {period}
+                </MenuItem>
+              )
+            )}
+          </TextField>
+        </div>
       </div>
       <Table>
-      <TableHead>
-  <TableRow>
-    <TableCell sx={{ fontWeight: "700" }}>EmpID</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>StaffName</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>Department</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>District</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>Period</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>Work Hours</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>Leave Hours</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>Total Hours</TableCell>
-    <TableCell sx={{ fontWeight: "700" }}>Actions</TableCell>
-  </TableRow>
-</TableHead>
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ fontWeight: "700" }}>EmpID</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>StaffName</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>Department</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>District</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>Period</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>Work Hours</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>Leave Hours</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>Total Hours</TableCell>
+            <TableCell sx={{ fontWeight: "700" }}>Actions</TableCell>
+          </TableRow>
+        </TableHead>
         <TableBody>
           {paginatedTimesheets.map((entry) => (
             <TableRow key={entry.employee_id}>
@@ -341,7 +423,6 @@ const TimesheetReports = () => {
                 <InfoIcon />
                 </IconButton> */}
                 <IconButton onClick={() => handleDownload(entry)}>
-                 
                   <DownloadForOfflineIcon />
                 </IconButton>
               </TableCell>
@@ -351,24 +432,23 @@ const TimesheetReports = () => {
       </Table>
 
       <TablePagination
-  rowsPerPageOptions={[5, 10, 25]}
-  component="div"
-  count={filteredTimesheets.length}
-  rowsPerPage={rowsPerPage}
-  page={page}
-  onPageChange={handleChangePage}
-  onRowsPerPageChange={handleChangeRowsPerPage}
-/>
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={filteredTimesheets.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
 
       <PdfTimesheetTable data={filteredData} />
-        
 
-<div id="content" className="hidden" >
-  <ViewTimesheet viewedTimesheet={viewedTimesheet} viewedpageTimesheet={viewedpageTimesheet} />
-  
-</div>
-  
-      
+      <div id="content" className="hidden">
+        <ViewTimesheet
+          viewedTimesheet={viewedTimesheet}
+          viewedpageTimesheet={viewedpageTimesheet}
+        />
+      </div>
     </div>
   );
 };
