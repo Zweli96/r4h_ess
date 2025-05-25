15d4c9c4678e7e5354d927bd5e7b7dfe2a476c70
Zwelithini Golowa <zweli@Zwelithinis-MacBook-Pro.local>
2025-05-25 21:33:17 +0200

    added redirect to signin page

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,7 +1,17 @@
 "use client";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 import * as React from "react";
 import SignInSide from "@../../../components/SignInSide";
 
 export default function SignInPage() {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      redirect("/dashboard");
+    }
+  }, [status]);
   return <SignInSide />;
 }
